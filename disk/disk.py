bfbab1415fa415c60f730a397db14c3e64538ef0
Dale Lakes <spitfire94155@gmail.com>
2016-04-18 13:52:53 -0400

    Started creating Disk component of tool

--- disk/disk.py
+++ disk/disk.py
@@ -7,6 +7,31 @@ class Disk():
     def __init__(self, filename):
         print "Hello, I am disk"
         self.filename = filename
-    '''def identifyFileType(self):
-        #TODO: Use bash's file utility to identify file and print
-    '''
+        self.diskType = self.identifyFileType()
+                
+        print "The first tool we will use is binwalk\n"
+        self.runBinWalk()
+        print "Now we will use Sleuthkit\n"
+        self.runSleuthkit()
+        
+    def identifyFileType(self):
+        print "What is the filetype of your file?\n"
+        print "1) RAW disk image"
+        print "2) ext2 disk image"
+        print "3) ext3 disk image"
+        print "4) ext4 disk image"
+        print "5) Fat32 disk image"
+        print "6) NTFS disk image"
+        print "7) HFS disk image"
+        print "8) Unknown"
+        types = {1: "raw", 2: "ext2", 3: "ext3", 4: "ext4", 5 : "fat32", 6: "ntfs", 7: "hfs", 8: "unknown" }
+        t = raw_input("")
+        try:
+            t = int(t)
+            if t > 0 and t < 8:
+                return types[t]
+            if t == 8:
+                print "Ok, let me figure it out for you..."
+                #TODO: disk type analysis tool will run 
+                
+        
